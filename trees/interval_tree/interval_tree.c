d39c6e64acb30751d7494d55ff2d3102d0983826
vivekh <vivek.hajela@yahoo.com>
2016-01-12 00:13:48 -0800

    commit

--- trees/interval_tree/interval_tree.c
+++ trees/interval_tree/interval_tree.c
@@ -45,7 +45,7 @@ int is_interval_overlap(INTVL *intvl1, int low, int high){
 /*inorder printing of interval tree*/
 void print_interval_tree(INTVL *intvl) {
 
-    if(!intvl)
+   if(!intvl)
 	return;
 
     print_interval_tree(intvl->left);
@@ -127,12 +127,53 @@ void merge_interval(INTVL *intvl, int low, int high) {
  * keep on adding appt slots into the interval tree and detect overlap while adding. This can be done in O(log(n)) time. 
  */
 
+
+/*print the values from low to high*/
+void print_interval(int low, int high){
+    int i;
+    for(i=low;i<=high;i++){
+	printf(" %d ", i);
+    }
+} 
+
+void find_coverage(INTVL *intvl, int *low, int *high) {
+
+    if(!intvl)
+	return;
+
+    find_coverage(intvl->left, low, high);
+
+    if(!(*low) && !(*high)){
+	*low = intvl->low;
+	*high = intvl->high;
+	print_interval(*low, *high);
+    } else {
+
+	if(is_interval_overlap(intvl, *low, *high)) {
+	    //if the current and previous interval overlaps and the high of next interval is greater then prev high, then just print the difference between prev and 
+	    //next high
+	    if(intvl->high > *high){
+		print_interval(*high+1, intvl->high);
+		*high = intvl->high;
+		*low = intvl->low;
+	    }
+	} else {
+	    //if the interval does not overlap then just print the interval
+	    print_interval(intvl->low, intvl->high);
+	    *high = intvl->high;
+	    *low = intvl->low;
+	}
+    }
+
+    find_coverage(intvl->right, low, high);
+}
+
 int main() {
 
     char c;
     INTVL *result;
     int choice;
-    int low, high;
+    int low = 0, high = 0;
 
     do {
 	printf("MENU OPTIONS\n");
@@ -172,6 +213,10 @@ int main() {
 		printf("Overlapping interval to [%d  %d] : [%d  %d]\n", low, high, result->low, result->high);
 		else 
 		printf("Overlapping interval to [%d  %d] : NULL\n", low, high);
+		break;
+
+	    case 4:
+		find_coverage(root, &low, &high);
 		break;
 
 	    default:
