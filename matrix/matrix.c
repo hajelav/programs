5890230fe51823cd160f03c685fa49a05e9691ea
vivekh <vivek.hajela@yahoo.com>
2016-01-17 21:18:43 -0800

    commit

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -80,6 +80,66 @@ void search_matrix(int **A, int row, int col) {
 
 }
 
+
+/**
+ * linkedin
+ * http://www.careercup.com/question?id=6482755168763904
+ *
+ ** Given a matrix of following between N LinkedIn users (with ids from 0 to N-1): 
+ ** followingMatrix[i][j] == true if user i is following user j 
+ ** thus followingMatrix[i][j] doesn't imply followingMatrix[j][i]. 
+ ** Let's also agree that followingMatrix[i][i] == false 
+ ** 
+ ** Influencer is a user who is: 
+ ** - followed by everyone else and 
+ ** - not following anyone himself 
+ ** 
+ ** This method should find an Influencer by a given matrix of following, 
+ ** or return -1 if there is no Influencer in this group. 
+
+# Example of m users(0, 1, 2,3)
+m = [[0, 1, 1, 0],
+     [1, 0, 1, 1],
+     [0, 0, 0, 0],
+     [1, 1, 1, 0]]
+
+  influencer : 2 since he is not following anyone and is being followed by 0, 1, 3
+ **/ 
+
+int get_influencer() {
+
+    int **follow;
+    int i, j, users;
+    int count=0;
+
+    printf("enter number of users\n");
+    scanf("%d", &users);
+
+    /*create follow array*/
+    follow = create_2Darray(users, users);
+    input_2Darray(follow, users, users);
+
+    /*
+     *to find out influencer
+     *    1. first we find out the user who is not following anyone, ie we should find row with all zeros(say ith row)
+     *    2. next if we find a user in step 1, now we find if all other users are following him, ie: ith colum should have all 1(except ith row)
+     */
+
+
+    /*step :1*/
+    for(i=0;i<users;i++){
+	for(j=0;j<users;j++){
+
+	    if(i!=j && follow[i][j] == 0 && follow[j][i] == 1)
+		count++;
+	}
+	if(count==users-1)
+	    return i;
+	count = 0;
+    }
+    return -1;
+}
+
 int main(){
     char c;
     int choice;
@@ -89,8 +149,9 @@ int main(){
 
 	printf("MENU OPTIONS\n");
 	printf("1 -- Rotate matrix by 90 degree\n");
-	printf("2 -- Make entire row and col zero , if you find an element as zero\n");
-	printf("2 -- Search in a 2D matrix whose rows and columns are sorted\n");
+	printf("2 -- Make entire row and column zero , if you find an element as zero\n");
+	printf("3 -- search in a 2D matrix whose rows and columns are sorted\n");
+	printf("4 -- get influencer\n");
 	printf("\n");
 	printf("Enter your choice\n");
 	scanf("%d",&choice);
@@ -124,6 +185,13 @@ int main(){
 		input_2Darray(A, row, col);
 		search_matrix(A, row, col);
 		break;
+
+	    case 4:
+		printf("influencer : %d\n", get_influencer());
+		break;
+
+	    default:
+		printf("invalid option\n");
 
 	}
 	printf("\n\n");
