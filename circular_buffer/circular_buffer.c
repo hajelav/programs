52c25de7f40adcc2d6d4cdca76fd42a172887422
vivekh <vivek.hajela@yahoo.com>
2016-02-01 22:05:40 -0800

    commit

--- circular_buffer/circular_buffer.c
+++ circular_buffer/circular_buffer.c
@@ -3,11 +3,11 @@
 #define CIRCULAR_BUF_SIZE 5
 
 typedef struct _cbuffer {
-    int start; //pointer for dequeuing the buffer
-    int end;   //pointer for queuing the buffer
+    int start; //pointer for dequeuing the buffer, we only delete using start pointer
+    int end;   //pointer for queuing the buffer, we only add using end pointer
     int *cbuf; //array for circular buffer
     int size;  //size of circular buffer
-
+    int curr_size; //current size of circular buffer
 } CBUFFER;
 
 CBUFFER* init_circular_buf() {
@@ -21,6 +21,7 @@ CBUFFER* init_circular_buf() {
     /*initialize start and end pointers*/
     cb->start = 0;
     cb->end = 0;
+    cb->curr_size = 0;
 
     /*initialize the size */
     cb->size = CIRCULAR_BUF_SIZE;
@@ -33,29 +34,46 @@ CBUFFER* init_circular_buf() {
     return cb;
 }
 
+/*add element to the queue*/
 void enqueue(CBUFFER *cb, int elem) {
 
     if(!cb || !cb->cbuf || cb->size <=0)
 	return;
+
+    /*update the current size as long as it is less than the buffer size*/
+    if(cb->curr_size < cb->size)
+	cb->curr_size++;
+
 
     cb->end = cb->end%cb->size;
     cb->cbuf[cb->end] = elem;
     cb->end++;
 }
 
+/*delete element from the queue*/
 void dequeue(CBUFFER *cb) {
 
     if(!cb || !cb->cbuf)
 	return;
 
-    if(cb->cbuf[cb->start] == 0){
+    /*if the current size becomes zero, then buffer is empty*/
+    if(cb->curr_size == 0){
 	printf("queue is empty\n");
-
-	/*reset the pointers when queue is empty*/
-	cb->end = cb->start;
-	/*cb->start = 0;*/
 	return;
     }
+
+    /*
+     *if the curent size becomes equal to the buffer size and start and end pointers are different,
+     *that means the buffer is full and overwritten. so we just need delete the elements from the 
+     *next position of that last added entry. We therefore point start to end(which is pointing to
+     *the next of last added entry
+     */
+    if(cb->curr_size == cb->size && cb->start!=cb->end){
+	cb->start = cb->end;
+    }
+
+    /*decrease the current size of buffer*/
+    cb->curr_size--;
 
     cb->start = cb->start%cb->size;
     cb->cbuf[cb->start] = 0;
