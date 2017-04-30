bbb1d501744b2c57d67df63dba0d12756456f72a
vivekh <vivek.hajela@yahoo.com>
2017-04-30 12:47:43 -0700

    commit

--- misc/misc.c
+++ misc/misc.c
@@ -898,9 +898,14 @@ int buy_sell_stock_single(int *A, int len) {
 }
 
 /*
- *leetcode problem 122
- *https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-  Idea: In this problem, since we can buy and sell the stock multiple times, that max profit would be 
+ *leetcode problem 122 : Best Time to Buy and Sell Stock II 
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/#/description
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Idea: In this problem, since we can buy and sell the stock multiple times, that max profit would be 
   sum of all +ve price difference
  */
 
