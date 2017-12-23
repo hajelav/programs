32f5aabf9473f0afe02c5fa7df39b9c2910bdf58
VivekH <vivek.hajela@yahoo.com>
2017-12-22 17:39:59 -0800

    commit

---
+++ backtracking/permutStrings.py
@@ -0,0 +1,41 @@
+#!/bin/python
+import sys
+#https://www.youtube.com/watch?v=uFJhEPrbycQ&list=PLFE6E58F856038C69&index=9
+'''
+In permutation/combination type of problems, always take two strings
+1. originalString
+2.ProcessedSoFar
+
+Try to think recursively solving the problem by moving the chars from original
+to processed string
+'''
+
+def permutationsOfString(originalString, processedSoFar) :
+
+    subString = ""
+
+    if(not originalString):
+        print processedSoFar
+        return
+
+    for i in range(len(originalString)):
+        #pop the char at index i from original list and append it to the other list
+        processedSoFar.append(originalString[i])
+        originalString.pop(i)
+
+        permutationsOfString(originalString, processedSoFar)
+
+        c = processedSoFar.pop()
+        originalString.insert(i, c)
+
+
+
+
+
+if __name__ == "__main__":
+
+    print 'enter string to permut'
+    originalString = list(raw_input())
+    processedSoFar = []
+    permutationsOfString(originalString, processedSoFar)
+
