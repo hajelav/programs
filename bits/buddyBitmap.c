9d480fca455eb973ee772a32dc63de7c74a20842
hajelav <vivek.hajela@yahoo.com>
2019-08-11 05:07:41 -0700

    commit

--- bits/buddyBitmap.c
+++ bits/buddyBitmap.c
@@ -171,23 +171,85 @@ void clearBit(int *A, int off, int len, int sizeA){
 }
 
 
+/*
+ *this is the API to set the bit in the bitmap. It returns the index of the first available bit which 
+ *can be set. 
+ */
+
+
+
+int set_bit( int *A, int size, int i) {
+
+    int left, right;
+
+    /*if the bit is already set then return*/
+    if(A[i] == 1)
+        return -1;
+
+    /*if you reach to an index which doesnt exist , then return*/
+    if(i >= size)
+        return -1;
+
+    /*recurse the left subtree*/
+    left = set_bit(A, size, 2*i+1);
+    /*recurse the right subtree*/
+    right = set_bit(A, size, 2*i+2);
+
+    /*if left subtree is filled and right has an empty slot*/
+    if((left < 0) && (right > 0))
+        return right;
+    /*if right subtree is filled and left has an empty slot*/
+    else if((right < 0) && (left > 0))
+        return left;
+    /*if both left and right subtree has empty slots*/
+    else if((left > 0) && (right > 0))
+        return left;
+    /*if an empty slot exists, then return its index*/
+    else if(A[i] == 0)
+        return i;
+    else
+        return -1;
+
+}
+
+
 int main() {
 
-    int n, i;
+    int i;
     int *A;
+    int idx;
 
-    printf("enter array len\n");
-    scanf("%d", &n);
+    /*printf("enter array len\n");*/
+    /*scanf("%d", &n);*/
 
-    A =  (int*)malloc(sizeof(int)*n);
-    printf("enter array\n");
-    for(i=0;i<n;i++){
-        scanf("%d", &A[i]);
+    A =  (int*)malloc(sizeof(int)*12);
+    for(i=0; i< 12; i++) {
+        A[i] = 0;
     }
-    print_1Darray(A, n);
+        
+    /*
+     *printf("enter array\n");
+     *for(i=0;i<n;i++){
+     *    scanf("%d", &A[i]);
+     *}
+     */
+    printf("\n");
+
+    print_1Darray(A, 12);
+    idx = set_bit(A, 12, 0);
+    printf("idx = %d\n", idx);
+    A[idx] = 1;
+    idx = set_bit(A, 12, 0);
+    printf("idx = %d\n", idx);
+    A[idx] = 1;
+    idx = set_bit(A, 12, 0);
+    printf("idx = %d\n", idx);
+    A[idx] = 1;
+    idx = set_bit(A, 12, 0);
+    printf("idx = %d\n", idx);
     //setBit(A, 9, 2, n);
-    clearBit(A, 1, 2, n);
-    print_1Darray(A, n);
+    /*clearBit(A, 1, 2, n);*/
+    print_1Darray(A, 12);
     free(A);
     return 0;
 }
