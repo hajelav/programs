d15507bd90d827b85121aafcfa8f022fb5c3de63
Vivek Hajela <vhajela@gmail.com>
2024-01-21 00:40:53 -0800

    commit

--- binary_search/types/src/main.cpp
+++ binary_search/types/src/main.cpp
@@ -28,9 +28,37 @@ int binSearchLeft(vector<int>& nums, int low, int high, int key) {
 
 }
 
+/*
+simple binary searh which -1 false when the element is not found
+else return the right index of the found element
+*/
+int binSearchRight(vector<int>& nums, int low, int high, int key) {
+
+    if(low > high)
+        return -1;
+
+    int mid = low + ((high-low)>>1);
+
+    if(low == high && key == nums[low])
+        return low;
+
+    if(key == nums[mid]) {
+        return binSearchLeft(nums, mid, high, key);   
+    } else if(key < nums[mid]) {
+        return binSearchLeft(nums, low, mid-1, key);
+    } else {
+        return binSearchLeft(nums, mid+1, high, key);
+    }
+
+}
+
 int main() {
 
     vector<int> V1 = {4,4,4,4,5,5,7,7,7};
+
+    /*******************************************
+     * TESTTING binSearchLeft
+    ********************************************/
     //search for element outside the arrray
     int idx1 = binSearchLeft(V1, 0, V1.size()-1, 3);
     cout <<  idx1 << endl;
