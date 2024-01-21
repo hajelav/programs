8b92586f49da094e41bcc6358b268cb1c0c743e6
Vivek Hajela <vhajela@gmail.com>
2024-01-21 00:35:20 -0800

    message

---
+++ binary_search/types/build/CMakeFiles/binarySearch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binarySearch.dir/src/main.cpp.o"
+  "bin/binarySearch"
+  "bin/binarySearch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binarySearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ binary_search/types/src/main.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+simple binary searh which -1 false when the element is not found
+else return the leftmost index of the found element
+*/
+int binSearchLeft(vector<int>& nums, int low, int high, int key) {
+
+    if(low > high)
+        return -1;
+
+    int mid = low + ((high-low)>>1);
+
+    if(low == high && key == nums[low])
+        return low;
+
+    if(key == nums[mid]) {
+        return binSearchLeft(nums, low, mid, key);   
+    } else if(key < nums[mid]) {
+        return binSearchLeft(nums, low, mid-1, key);
+    } else {
+        return binSearchLeft(nums, mid+1, high, key);
+    }
+
+}
+
+int main() {
+
+    vector<int> V1 = {4,4,4,4,5,5,7,7,7};
+    //search for element outside the arrray
+    int idx1 = binSearchLeft(V1, 0, V1.size()-1, 3);
+    cout <<  idx1 << endl;
+    int idx2 = binSearchLeft(V1, 0, V1.size()-1, 10);
+    cout <<  idx2 << endl;
+
+    //search for element present in the list
+    int idx3 = binSearchLeft(V1, 0, V1.size()-1, 4);
+    cout <<  idx3 << endl;
+    int idx4 = binSearchLeft(V1, 0, V1.size()-1, 7);
+    cout <<  idx4 << endl;
+
+    //searh for an element not present
+    int idx5 = binSearchLeft(V1, 0, V1.size()-1, 6);
+    cout <<  idx5 << endl;
+
+
+    return 0;
+}
